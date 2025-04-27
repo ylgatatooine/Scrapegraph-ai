f26a7a14b70bc38bdeb887ef4c3dfb453c65f875
ylgatatooine <122713205+ylgatatooine@users.noreply.github.com>
2025-04-27 12:04:49 -0500

    try llama

--- tutorials/hello-webscraping.py
+++ tutorials/hello-webscraping.py
@@ -10,6 +10,26 @@ graph_config = {
     "headless": False,
 }
 
+
+# from dotenv import load_dotenv
+# import os
+# # Load environment variables from a .env file
+# load_dotenv()
+
+# # Get the OpenAI API key from the .env file
+# api_key = os.getenv("OPENAI_API_KEY")
+# if not api_key:
+#     raise ValueError("OPENAI_API_KEY is not set in the .env file")
+
+# graph_config = {
+#    "llm": {
+#        "api_key": api_key,
+#        "model": "openai/gpt-4o-mini",
+#    },
+#    "verbose": True,
+#    "headless": False,
+# }
+
 # Create the SmartScraperGraph instance
 smart_scraper_graph = SmartScraperGraph(
     prompt="Extract useful information from the webpage, including a description of what the company does, founders and social media links",
@@ -20,5 +40,6 @@ smart_scraper_graph = SmartScraperGraph(
 # Run the pipeline
 result = smart_scraper_graph.run()
 
+# Run the pipeline
 import json
 print(json.dumps(result, indent=4))
